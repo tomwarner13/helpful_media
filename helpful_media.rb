45a1e2930bde7f47265b2d9a0a82931d48cb4274
tom warner <tomwarner13@gmail.com>
2016-08-14 16:45:57 -0400

    Fleshing out the skeleton

--- helpful_media.rb
+++ helpful_media.rb
@@ -4,27 +4,38 @@ require 'haml'
 
 class HelpfulMedia < Sinatra::Base
   get '/' do
+    @title = "Home"
     haml :index
   end
 
   get '/about' do
+    @title = "About Us"
     haml :about
   end
 
   get '/faq' do
+    @title = "Frequently Asked Questions"
     haml :faq
   end
 
   get '/people' do
+    @title = "People"
     haml :people
   end
 
   get '/people/:name' do
     @person = params["name"]
+    @title = @person
     haml :person 
   end
 
+  get '/contact' do
+    @title = "Contact Us"
+    haml :contact
+  end
+
   not_found do
+    @title = "404"
     haml :notfound
   end
 end
