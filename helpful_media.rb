ad457ed64b057e8f2631b3e812acbf3d05a95b32
tom warner <tomwarner13@gmail.com>
2016-08-13 20:13:33 -0400

    Skeleton structure complete, plus some proofs of concept

--- helpful_media.rb
+++ helpful_media.rb
@@ -10,5 +10,22 @@ class HelpfulMedia < Sinatra::Base
   get '/about' do
     haml :about
   end
+
+  get '/faq' do
+    haml :faq
+  end
+
+  get '/people' do
+    haml :people
+  end
+
+  get '/people/:name' do
+    @person = params["name"]
+    haml :person 
+  end
+
+  not_found do
+    haml :notfound
+  end
 end
 
